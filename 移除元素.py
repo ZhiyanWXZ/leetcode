3b4ba46deddeaed44d6812d085a5ed903f47a73b
ZhiyanWXZ <1050642597@qq.com>
2021-03-13 22:17:59 +0800

    2021/3/13

--- 移除元素.py
+++ 移除元素.py
@@ -1,8 +1,10 @@
 from typing import List
 
 # python  the difference of del and remove:
-# 1、del根据索引进行删除，不会根据值进行删除
-# 2、remove方法会将list中首次出现的元素删除，按值删除。但在程序中，使用的是下标遍历的方法，在删除一个元素后，下标和实际将要访问的元素发生错位，导致错误
+# 1、del deletes the member according to the index of the list,not the value.
+# 根据索引进行删除，不会根据值进行删除
+# 2、function remove deletes the first occurence of the value in the list, also, it delete the member by the value.
+# list中首次出现的元素删除，按值删除。但在程序中，使用的是下标遍历的方法，在删除一个元素后，下标和实际将要访问的元素发生错位，导致错误
 def removeElement(nums: List[int], val: int) -> int:
 
     while val in nums:
@@ -13,7 +15,7 @@ def removeElement(nums: List[int], val: int) -> int:
 nums = [0,1,2,2,3,0,4,2]
 val = 2
 
-#这种方式下
+# in this way, del can not delete the target value
 for num in nums:
     if num == val:
         del num
